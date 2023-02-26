e480e32c957e5eeeedbb235683251b3eee7e5a2d
niilolehtonen <niilo.i.lehtonen@gmail.com>
2023-02-26 19:53:54 +0200

    error handling for 'add release' and 'add review' forms

--- routes.py
+++ routes.py
@@ -72,9 +72,26 @@ def releases():
         return render_template("releases.html", release_info=release_info)
     if request.method == "POST":
         song_name = request.form["song_name"]
+        if song_name == "":
+            return render_template("error.html", message="Empty song name")
+        if len(song_name) > 50:
+            return render_template("error.html", message="Song name too long")
         album_name = request.form["album_name"]
+        if album_name == "":
+            return render_template("error.html", message="Empty album name")
+        if len(album_name) > 50:
+            return render_template("error.html", message="Album name too long")
         artist_name = request.form["artist_name"]
-        year = int(request.form["year"])
+        if artist_name == "":
+            return render_template("error.html", message="Empty artist name")
+        if len(artist_name) > 50:
+            return render_template("error.html", message="Artist name too long")
+        year = (request.form["year"])
+        if year.isdigit() == False:
+            return render_template("error.html",message="Provide a valid year")
+        if int(year) < 1 or int(year) > 2023:
+            return render_template("error.html",message="Provide a valid year")  
+        int(year) 
         db.session.execute(text("""
             INSERT INTO releases (song, artist, album, year)
             VALUES (:song_name, :artist_name, :album_name, :year)
@@ -116,7 +133,13 @@ def review(song_name):
         return render_template("review.html",reviews=reviews, song_name=song_name)
     if request.method == "POST":
         rating = request.form['rating']
+        if not rating.isdigit() or int(rating) not in [1,2,3,4,5]:
+            return render_template("error.html",message="Provide a rating (1, 2, 3, 4 or 5)")
         comment = request.form['comment']
+        if comment == "":
+            return render_template("error.html",message="Provide a comment")
+        if len(comment) > 50:
+            return render_template("error.html",message="Comment too long")
         user_id = users.user_id()
         db.session.execute(text("""INSERT INTO reviews (song_name, rating, comment, user_id) VALUES (:song_name, :rating, :comment, :user_id)"""),params={"song_name": song_name, "rating": rating, "comment": comment, "user_id": user_id})
         db.session.commit()
