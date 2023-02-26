f93ee38587009d48ae3685c1b2dfddfe1ec665b3
niilolehtonen <niilo.i.lehtonen@gmail.com>
2023-02-26 19:09:53 +0200

    added artist_releases table

--- schema.sql
+++ schema.sql
@@ -1,3 +1,9 @@
+DROP TABLE IF EXISTS artists CASCADE;
+DROP TABLE IF EXISTS releases CASCADE;
+DROP TABLE IF EXISTS reviews CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS artist_releases CASCADE;
+
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     name TEXT,
@@ -12,8 +18,6 @@ CREATE TABLE reviews (
     user_id INTEGER REFERENCES users ON DELETE CASCADE
 );
 
-
-
 CREATE TABLE releases (
     id SERIAL PRIMARY KEY,
     song TEXT,
@@ -25,6 +29,11 @@ CREATE TABLE releases (
 
 CREATE TABLE artists (
     id SERIAL PRIMARY KEY,
-    name TEXT,
-    release_id INTEGER REFERENCES releases ON DELETE CASCADE 
+    name TEXT
+);
+
+CREATE TABLE artist_releases (
+    artist_id INTEGER REFERENCES artists(id) ON DELETE CASCADE,
+    release_id INTEGER REFERENCES releases(id) ON DELETE CASCADE,
+    PRIMARY KEY (artist_id, release_id)
 );
